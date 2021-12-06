88aff6a105cddd71feb4f7baf501e5c12b275ed3
Abner <abner.rodrigues@zooxsmart.com>
2021-12-06 17:57:37 -0300

    chore: alterando service worker

--- push.js
+++ push.js
@@ -11,5 +11,13 @@ push.setVapidDetails(
     vapIdKeys.privateKey
 );
 
-const sub = {};
+const sub = {
+    "endpoint": "https://fcm.googleapis.com/fcm/send/cE9XdEPKIsY:APA91bF_A9b1bSeIBJbrCoMGsYasD2UCfH2mIEedJ0besBFjOwnXSYG4EfrVqzEJc3E8aDXQFBQtZb6CVQ-40vB_pVz0Ne3BD6qKPqlZQTfsS03822EkQ6lFV6l3SzcwI289zT0EOr7R",
+    "expirationTime": null,
+    "keys": {
+        "p256dh": "BCqTCWCMfW_5vH82xDR8-OWm27C-af6dRn50L18xGiyESNfNEGJu9UVeyKFGrAUfVc1Sw0woKJRFoYK5TJG1iko",
+        "auth": "-vo84qM_HlgyY5CszPuzDw"
+    }
+}
+
 push.sendNotification(sub, "Testeeeee")
--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener("push", () => {    
-    self.registration.sendNotification("oi", {});
+self.addEventListener("push", (data) => {
+    console.log("Push data received", data);
+    
+    const data = {
+        title: "Push Notification",
+        content: "Click to see the notification",
+        openUrl: "/"
+    };
+    
+    const options = {
+        body: data.content,
+        icon: "/src/images/icons/app-icon-96x96.png",
+        badge: "/src/images/icons/app-icon-96x96.png",
+        data: {
+        url: data.openUrl
+        }
+    };
+    
+    self.registration.showNotification(data.title, options);
 })
