c0183c96c4d12186f71c8fb5f65bd1a9207d5626
Abner <abner.rodrigues@zooxsmart.com>
2021-12-06 17:50:16 -0300

    chore: add data files

--- push.js
+++ push.js
@@ -3,10 +3,13 @@ const push = require("web-push");
 const vapIdKeys = {
     publicKey: 'BIYU9-MdUzL3bYjmOwuEeBlea33V18PoZ8wkMXU9qcYh1pc5dS63PVf3TEWh-mRJjnxUOZA1bexDbDcRjnoeKlU',
     privateKey: 'ssWDgyb9MU_vK8PiRnzdkGxlFWafZuMdA5RdS60sUZA'
-
 }
 
-push.setVapidDetails("mailto:test@abner.com", vapIdKeys.publicKey, vapIdKeys.privateKey);
+push.setVapidDetails(
+    "mailto:test@abner.com",
+    vapIdKeys.publicKey,
+    vapIdKeys.privateKey
+);
 
 const sub = {};
 push.sendNotification(sub, "Testeeeee")
--- sw.js
+++ sw.js
@@ -1,18 +1,3 @@
-self.addEventListener("push", () => {
-    const data = {
-        title: "Push Notification",
-        content: "Click to see the notification",
-        openUrl: "/"
-    };
-    
-    const options = {
-        body: data.content,
-        icon: "/src/images/icons/app-icon-96x96.png",
-        badge: "/src/images/icons/app-icon-96x96.png",
-        data: {
-        url: data.openUrl
-        }
-    };
-    
-    self.registration.sendNotification(data.title, options);
+self.addEventListener("push", () => {    
+    self.registration.sendNotification("oi", {});
 })
