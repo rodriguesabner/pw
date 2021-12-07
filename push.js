b2052493926291d6d564c712dbd82ef208a31809
Abner <abner.rodrigues@zooxsmart.com>
2021-12-07 09:57:51 -0300

    chore: ajustando funcoes

--- push.js
+++ push.js
@@ -12,12 +12,12 @@ push.setVapidDetails(
 );
 
 const sub = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/cE9XdEPKIsY:APA91bF_A9b1bSeIBJbrCoMGsYasD2UCfH2mIEedJ0besBFjOwnXSYG4EfrVqzEJc3E8aDXQFBQtZb6CVQ-40vB_pVz0Ne3BD6qKPqlZQTfsS03822EkQ6lFV6l3SzcwI289zT0EOr7R",
-    "expirationTime": null,
-    "keys": {
-        "p256dh": "BCqTCWCMfW_5vH82xDR8-OWm27C-af6dRn50L18xGiyESNfNEGJu9UVeyKFGrAUfVc1Sw0woKJRFoYK5TJG1iko",
-        "auth": "-vo84qM_HlgyY5CszPuzDw"
+    endpoint: "https://fcm.googleapis.com/fcm/send/csoO-3B_2xk:APA91bFgYdLyt_x798DAueXesPfGBRgxI-ut23hbaMv3sOHgd9KOq4_hjTLOuottiktXO0s5jqn5yLJyXd-j61xsdzeJ2KQH-8Zqf69LGG0FdKAftupy_6ld0JccXe7dpKXLhlG7uuVZ",
+    expirationTime: null,
+    keys: {
+        p256dh: "BPfByE0cXtET1grOjTDZPzuGNhl3FlJJqOG70CX1GsW9m6mbR5N1MBWzBnUiFzbAY1nHAqyLYnmDBBHbzQiKGCE",
+        auth: "JcKEyL55J0uSxwdkCNIaNQ"
     }
 }
 
-push.sendNotification(sub, "Testeeeee")
+push.sendNotification(sub, "Testeeeee");
